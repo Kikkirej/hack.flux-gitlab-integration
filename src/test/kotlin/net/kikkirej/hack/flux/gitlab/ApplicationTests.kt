314b73b8af0ca82f56f53b4a0ca3d168538fc734
Kikkirej <5462557+Kikkirej@users.noreply.github.com>
2026-07-25 18:28:49 +0200

    :tada: Spring Initalizr Content

---
+++ src/test/kotlin/net/kikkirej/hack/flux/gitlab/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.kikkirej.hack.flux.gitlab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
